23473d88cd6f7a6a4778c2efffa071671b31eb79
Oliver O'Halloran <oohall@gmail.com>
2019-08-11 18:37:17 +1000

    inital FDT parsing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-//extern crate fdt;
-
 #![feature(lang_items)]
 #![feature(core_intrinsics)]
 #![no_std]
 #![no_main]
 
+extern crate fdt;
 
 use core::intrinsics;
 use core::panic::PanicInfo;
+use core::cell::UnsafeCell;
 
 use core::fmt;
+use core::fmt::Write;
 use core::str;
+
+use fdt::FDT;
 
 extern {
     fn ohshit();
@@ -126,21 +129,46 @@ pub fn memcmp(s1 : *mut u8, s2 : *const u8, size : isize) -> isize
 
 #[no_mangle]
 pub fn _start(fdt_ptr : u64) -> ! {
-//    let mut cons = Console {};
+    let mut cons = Console {};
+    let fdt;
 
     init_sio();
 
-    prlog("test\r\n");
+    write!(&mut cons, "FDT at: {:016x}\r\n", fdt_ptr);
 
-    panic!("fuck");
-    /*
-    fmt::write(&mut cons, format_args!("hello \r\n", "world")).unwrap();
-//    fmt::write(&mut cons, format_args!("hello \r\n")).unwrap();
+    unsafe {
+        fdt = FDT::from_raw(fdt_ptr as *const u8).unwrap();
+    }
 
-    prlog("tested\r\n");
+    for n in fdt.nodes() {
+        let sn = n.size_cells();
+        let an = n.address_cells();
 
-    loop{}
-    */
+        write!(&mut cons, "node: {} [{},{}]\r\n", n.name(), an, sn);
+
+        for prop in n.properties() {
+            if prop.name() == "reg" && sn > 0 && an > 0 {
+                let mut size = 0_u64;
+                let mut addr = 0_u64;
+
+                for i in 0..sn {
+                    size <<= 32;
+                    size |= prop.cell(i as usize).unwrap_or(0u32) as u64;
+                }
+
+                for i in 0..an {
+                    addr <<= 32;
+                    addr |= prop.cell(i as usize).unwrap_or(0_u32) as u64;
+                }
+
+                write!(&mut cons, "\treg: [{},{}] = [{:016x}, {:016x}]\r\n", sn, an, addr, size);
+            }
+            write!(&mut cons, "\tprop: {}\r\n", prop.name());
+        }
+    }
+
+
+    loop{};
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
